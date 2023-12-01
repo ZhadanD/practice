7f13ede3a8eeb7821175b7a57a4b2cefd50aaca1
ZhadanD <zhdv07@mail.ru>
2023-12-01 15:30:43 +0300

    Закончил все задания в 13-й лабораторной работе;

---
+++ 13/task_3/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#but1').click(function () {
+        $('#test').hide();
+    });
+
+    $('#but2').click(function () {
+        $('#test').show();
+    });
+})
